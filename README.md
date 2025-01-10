2f837be8f52d83ef9af04a785e5f66a8b3736959
Yotam <yotam@freename.io>
2025-01-10 13:46:08 +0200

    Getting ready to publish the package

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# freename-resolution-snap
+# Freename Resolution Snap
+
+Metamask Snap implmentation of the Freename Resolution API.
+Installing this snap adds to Metamask the ability to resolve domains across many providers (Freename, ENS, Unstoppable domains, etc) to their respective onchain addresses.
+
+## Local development
+
+Start by installing the dependencies:
+
+```bash
+yarn install
+```
+
+Then, you can start the development server:
+
+```bash
+yarn start
+```
+
+The snap then will be available locally with the snapID `local:http://localhost:8080`.
---
+++ src/index.tsx
@@ -0,0 +1,74 @@
+import type { OnNameLookupHandler } from '@metamask/snaps-sdk';
+
+const registryToProtocol = {
+  "FNS": "Freename",
+  "ENS": "Ethereum Name Service",
+  "UD": "Unstoppable Domains",
+  "BNS": "Base Name Service",
+  "ID": "SpaceID",
+  "DNAME": "DecentraName"
+};
+
+const chainIdToName = {
+  "eip155:1": "ethereum",
+  "eip155:137": "polygon",
+  "eip155:8453": "base",
+  "eip155:56": "bsc",
+  "eip155:1329": "sei",
+  "eip155:42793": "etherlink"
+};
+
+/**
+ * @param request - The request arguments.
+ * @param request.domain - The domain to resolve. Will be undefined if an address is provided.
+ * @param request.address - The address to resolve. Will be undefined if a domain is provided.
+ * @param request.chainId - The CAIP-2 chain ID of the associated network.
+ * @returns If successful, an object containing the resolvedAddress. Null otherwise.
+ */
+export const onNameLookup: OnNameLookupHandler = async (request) => {
+  const { chainId, domain } = request;
+  const baseURL = "https://apis.freename.io/api/v1/resolver";
+  const supportedChains = Object.keys(chainIdToName);
+  const supportedRegistries = Object.keys(registryToProtocol);
+
+  if (!domain || !supportedChains.includes(chainId)) {
+    return null;
+  }
+
+  for (const registry of supportedRegistries) {
+    try {
+      const response = await fetch(`${baseURL}/${registry}/${domain}`);
+
+      if (!response.ok) {
+        console.error(`Failed to fetch from registry ${registry}: ${response.statusText}`);
+        continue;
+      }
+
+      const resJson = await response.json();
+
+      const resolvedAddress = resJson?.data?.resolvedAddress;
+      if (resolvedAddress) {
+        const network = resJson?.data?.network?.toLowerCase();
+        const protocolName = `${registryToProtocol[registry as keyof typeof registryToProtocol]} (${network})`;
+
+        const protocol = chainIdToName[chainId as keyof typeof chainIdToName] !== network
+          ? `⚠️ ${protocolName}`
+          : protocolName;
+
+        return {
+          resolvedAddresses: [
+            {
+              resolvedAddress,
+              protocol,
+              domainName: domain,
+            },
+          ],
+        };
+      }
+    } catch (error) {
+      console.error(`Error resolving domain for registry ${registry}:`, error);
+    }
+  }
+
+  return null;
+};
